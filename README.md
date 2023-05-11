7482bfc884d67356b707c08fe2f605f6f88b9487
andyyang352 <40815271+andyyang352@users.noreply.github.com>
2023-05-11 11:04:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xqfg2k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-iwdcry)
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Instructions from './Instructions';
+import './style.css';
+
+export default function App() {
+  return <Instructions />;
+}
